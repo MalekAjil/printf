a16130f6a6eee5452c0ccf114630cc647e53256c
MalekAjil <aboanasajil@gmail.com>
2023-08-20 11:51:59 -0700

    printf2

---
+++ README.md
@@ -0,0 +1,2 @@
+This is the printf project of ALX SE program
+which I and my friend Yusuf Salama programmed it.
--- main.h
+++ main.h
@@ -21,6 +21,15 @@ int _printf(const char *const format, ...);
 int print_char(va_list arg);
 int print_string(va_list arg);
 int print_percent(void);
-int print_int();
+int print_int(va_list arg);
+
+int print_dec(va_list arg);
+int print_d(int num);
+int print_oct(va_list arg);
+int print_o(unsigned int num);
+int print_hex(va_list arg);
+int print_x(unsigned int num, short upper);
+int print_bin(va_list arg);
+int print_b(unsigned int num);
 
 #endif /* MAIN_H */
