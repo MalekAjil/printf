5cc41c50aade0de397644a1c73e7d310d8407cbb
YusufYuso <yusuf.elsaed@gmail.com>
2023-08-22 09:58:03 +0300

    Updating C source, ASCII text file: print_int.c

--- print_int.c
+++ print_int.c
@@ -8,10 +8,41 @@
  */
 int print_int(va_list arg)
 {
-	int n = 0;
+	int n = va_arg(arg, int);
+	int num, last = n % 10, digit, exp = 1;
+	int  i = 1;
 
-	n = print_d(va_arg(arg, int));
-	return (n);
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (i);
 }
 
 /**
