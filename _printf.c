588af06e932d74a424ced7f09ed3c3470192e07b
YusufYuso <yusuf.elsaed@gmail.com>
2023-08-22 09:23:18 +0300

    Updating C source, ASCII text file: _printf.c

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@ int _printf(const char *format, ...);
 /**
  * prints - function that prints arguments
  * @format: the string
- * @arg: the arguments
+ *
  * Description - this function links operators to actions
  * Return: string length
  */
