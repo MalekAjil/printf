d9a451527b988e24e19f67e5308487fc7fd073e0
YusufYuso <yusuf.elsaed@gmail.com>
2023-08-22 10:30:03 +0300

    Updating C source, ASCII text file: print_int.c

--- print_int.c
+++ print_int.c
@@ -9,58 +9,35 @@
 int print_int(va_list arg)
 {
 	int n = va_arg(arg, int);
-	int num, last = n % 10, digit, exp = 1;
-	int  i = 1;
+	int num, digit, i;
+	int count = 1;
+	int l_digit = n % 10;
+	int exp = 1;
 
 	n = n / 10;
 	num = n;
 
-	if (last < 0)
+	if (l_digit < 0)
 	{
 		_putchar('-');
 		num = -num;
 		n = -n;
-		last = -last;
-		i++;
+		l_digit = -l_digit;
+		count++;
 	}
 	if (num > 0)
 	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
+		for (i = 0; num / 10 > i; exp *= 10, num /= 10)
+			;
 		num = n;
-		while (exp > 0)
+		for (i = 0; exp > i; exp = exp / 10, count++)
 		{
 			digit = num / exp;
 			_putchar(digit + '0');
 			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
 		}
 	}
-	_putchar(last + '0');
+	_putchar(l_digit + '0');
 
-	return (i);
-}
-
-/**
- * print_d - print decimal
- * @dec: decimal number
- * Return: number of printed bytes
- */
-int print_d(int dec)
-{
-	int n = 0;
-
-	if (dec < 0)
-	{
-		n += _putchar('-');
-		dec *= -1;
-	}
-	if (dec / 10)
-		n += print_d(dec / 10);
-	n += _putchar('0' + dec % 10);
-	return (n);
+	return (count);
 }
